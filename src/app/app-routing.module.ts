fd68504e3737916e282ec84ca8899c61e003e344
Josh Houck <Josh123090@gmail.com>
2018-07-26 21:35:51 -0400

    First iteration website

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule }               from '@angular/core';
+import { Routes, RouterModule }   from '@angular/router';
+import { HomeComponent }          from './components/home/home.component';
+import { AboutUsComponent }       from './components/about-us/about-us.component';
+import { GalleryComponent }       from './components/gallery/gallery.component';
+import { TermsComponent }         from './components/terms/terms.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent,
+  },
+  {
+    path: 'terms',
+    component: TermsComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule }   from '@angular/platform-browser';
-import { NgModule }        from '@angular/core';
+import { BrowserModule }       from '@angular/platform-browser';
+import { NgModule }            from '@angular/core';
 
+import { AppRoutingModule }    from './app-routing.module';
 
-import { AppComponent }    from './app.component';
+import { AppComponent }        from './app.component';
+import { HomeComponent }       from './components/home/home.component';
+import { AboutUsComponent }    from './components/about-us/about-us.component';
+import { GalleryComponent }    from './components/gallery/gallery.component';
+import { TermsComponent }      from './components/terms/terms.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    GalleryComponent,
+    TermsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
