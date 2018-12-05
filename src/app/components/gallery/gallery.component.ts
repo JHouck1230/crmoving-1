ff0c371abf46be1f4cbaabf596fd67097072cf3d
Josh Houck <Josh123090@gmail.com>
2018-12-05 17:40:24 -0500

    Mobile friendly

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class GalleryComponent {
 
 }
