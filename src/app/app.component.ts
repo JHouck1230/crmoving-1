7a7d6649db16346b1707ad252608c2aa1ea80a03
Josh Houck <Josh123090@gmail.com>
2020-03-11 19:09:31 -0400

    Adding conditional tab css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  activeTab: string;
+
+  ngOnInit() {
+    switch (document.location.hash) {
+      case '#/':
+        this.activeTab = 'home';
+        break;
+      case '#/about-us':
+        this.activeTab = 'about-us';
+        break;
+      case '#/gallery':
+        this.activeTab = 'gallery';
+        break;
+      case '#/terms':
+        this.activeTab = 'terms';
+        break;
+    }
+  }
+
 }
